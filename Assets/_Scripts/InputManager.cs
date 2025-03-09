bb912271abce06f9a361433c61de9779f345e79e
sjanr <97484144+sjanr@users.noreply.github.com>
2025-03-09 13:43:08 -0700

    Implemented working functionality for play and exit buttons

--- Assets/_Scripts/InputManager.cs
+++ Assets/_Scripts/InputManager.cs
@@ -6,6 +6,7 @@ public class InputManager : MonoBehaviour
     public UnityEvent<Vector2> OnMove = new();
     public UnityEvent OnJump = new();
     public UnityEvent OnDash = new();
+    public UnityEvent OnSettingsMenu = new();
     void Update()
     {
         Vector2 input = Vector2.zero;
@@ -34,6 +35,11 @@ public class InputManager : MonoBehaviour
         {
             OnDash?.Invoke();
         }
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            OnSettingsMenu?.Invoke();
+        }
+
         OnMove?.Invoke(input.normalized);
     }
 }
